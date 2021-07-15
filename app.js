25049890fdf43e68fe4b31d999eeb485b91153b4
Homero Meireles Junior <65316962+homeromeirelesjunior@users.noreply.github.com>
2021-07-15 14:13:35 -0300

    (15/07) refactoring and final fixes

--- app.js
+++ app.js
@@ -1,53 +1,38 @@
-/*
-Este exercício será um pouquinho diferente dos anteriores.
-
-Seu desafio é desenvolver uma versão do quiz que:
-
-- Aborda um tema diferente (não pode ser de filmes);
-- Tem um tema de cores diferente do que foi apresentado na aula;
-- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
-
-Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, tente fazer esse exercício sem rever partes da aula.
-
-É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
-
-Depois de fazer o que foi pedido acima, crie um repositório no GitHub para a sua aplicação e abra uma issue no repositório do curso com:
-
-- O link da sua versão do quiz;
-- Quais foram as suas maiores dúvidas ou dificuldades durante a execução desse exercício;
-- Quais foram as suas menores dificuldades durante a execução desse exercício.
-
-Link do repositório do curso: https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo/issues
-
-Ps: se você não conseguiu fazer tudo o que foi pedido acima, abra a issue mesmo assim =)
-*/
+const form = document.querySelector('.quiz-form')
+const finalScoreContainer = document.querySelector('.final-score-container')
 
 const correctAnswers = ['A', 'B', 'B', 'B']
-const form = document.querySelector('.quiz-form')
-const finalResult = document.querySelector('.result')
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
+let score = 0
 
-    let score = 0
+const getUserAnswers = () => {
+    let userAnswers = correctAnswers.map((_, index) => {
+        return form[`inputQuestion${index + 1}`].value
+    })
 
-    const userAnswers = [
-        form.inputQuestion1.value,
-        form.inputQuestion2.value,
-        form.inputQuestion3.value,
-        form.inputQuestion4.value,
-    ]
+    return userAnswers
+}
 
+const calculateUserScore = (userAnswers) => {
     userAnswers.forEach((userAnswer, index) => {
-        if (userAnswer === correctAnswers[index]) {
+        const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+
+        if (isUserAnswerCorrect) {
             score += 25
         }
     })
+}
 
-    scrollTo(0, 0)
+const showFinalScore = () => {
+    scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+    finalScoreContainer.classList.remove('d-none')
+}
 
-    finalResult.classList.remove('d-none')
-
+const animateFinalScore = () => {
     let counter = 0
 
     const timer = setInterval(() => {
@@ -55,7 +40,21 @@ form.addEventListener('submit', event => {
             clearInterval(timer)
         }
 
-        finalResult.querySelector('span').textContent = `${counter}%`
-        counter++
+        finalScoreContainer.querySelector('span').textContent = `${counter++}%`        
     }, 10)
+}
+
+const resetUserScore = () => {
+    score = 0
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const userAnswers = getUserAnswers()
+
+    resetUserScore()
+    calculateUserScore(userAnswers)
+    showFinalScore()
+    animateFinalScore()
 })
